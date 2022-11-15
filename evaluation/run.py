200a40b0eff8e110e97cea2de83e8fb6a2943215
Francisco Cerqueira <up201905337@fe.up.pt>
2022-11-15 18:56:27 +0000

    feat: add more queries

--- evaluation/run.py
+++ evaluation/run.py
@@ -12,7 +12,7 @@ for (query_name, version_name, url) in lines:
         continue
     Path(f'{query_name}/{version_name}').mkdir(parents=True, exist_ok=True)
     print(f'Evaluating {query_name}/{version_name}')
-    process = subprocess.Popen(['python3', 'evaluate.py', f'{query_name}/relevant.txt', url, f'{query_name}/{version_name}'])
+    process = subprocess.Popen(['python', 'evaluate.py', f'{query_name}/relevant.txt', url, f'{query_name}/{version_name}'])
     process.wait()
     index[query_name] = [version_name] if query_name not in index else index[query_name] + [version_name]
 
