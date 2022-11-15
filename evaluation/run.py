84721bab90b31f32181a4ae3348635b572454b85
Filipe Campos <filipepcampos24@gmail.com>
2022-11-15 18:13:06 +0000

    feat: optimize run.py

--- evaluation/run.py
+++ evaluation/run.py
@@ -15,7 +15,6 @@ for (query_name, version_name, url) in lines:
     Path(f'{query_name}/{version_name}').mkdir(parents=True, exist_ok=True)
     print(f'Evaluating {query_name}/{version_name}')
     process = subprocess.Popen(['python3', 'evaluate.py', f'{query_name}/relevant.txt', url, f'{query_name}/{version_name}'])
-    process.wait()
     index[query_name] = [version_name] if query_name not in index else index[query_name] + [version_name]
 
 # Build comparisons
